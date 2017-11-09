e9ce8c938813abd8c01e7c82ac05a75747ad0f35
Weizhen Huang <weizhen@128-148.eduroam.rwth-aachen.de>
2017-11-09 21:37:24 +0100

    minimum cmaketest

---
+++ app/hello.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "mymath.h"
+ 
+int main()
+{
+  std::cout << add(3,4) << std::endl;
+  return 0;
+}
+
+
+
---
+++ math/math.cc
@@ -0,0 +1,8 @@
+#include "mymath.h"
+#include <iostream>
+ 
+int add(int a, int b) {
+    return a + b;
+}
+
+
